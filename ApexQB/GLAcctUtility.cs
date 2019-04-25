670d097d54663d048d0cc9333eb870a2855df506
Phil Kramer <philk@vulcan-software.com>
2019-04-25 11:35:13 -0700

    Set CET G/L account names to match CEDB; Update to Devexpress 18.2

--- ApexQB/GLAcctUtility.cs
+++ ApexQB/GLAcctUtility.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace ApexQB
 {
-   public static class GLAcctUtility
-   {
-      public struct GLAcct
-      {
-         public string ApexCompany { get; set; }
-         public string ApexGLRef { get; set; }
-         public string QBGLAcctFullName { get; set; }
-      }
+    public static class GLAcctUtility
+    {
+        public struct GLAcct
+        {
+            public string ApexCompany { get; set; }
+            public string ApexGLRef { get; set; }
+            public string QBGLAcctFullName { get; set; }
+        }
 
-      public static List<GLAcct> GLAcctList;
+        public static List<GLAcct> GLAcctList;
 
-      public static List<GLAcct> BuildGLAcctList()
-      {
-         return new List<GLAcct>
+        public static List<GLAcct> BuildGLAcctList()
+        {
+            return new List<GLAcct>
          {
             //CEM, company "M"
             new GLAcct { ApexCompany = "M", ApexGLRef = "M", QBGLAcctFullName = "Cost of Goods Sold:MATERIALS:JOB MATERIALS - PO" },
@@ -29,11 +29,11 @@ namespace ApexQB
             new GLAcct { ApexCompany = "M", ApexGLRef = "E", QBGLAcctFullName = "Cost of Goods Sold:SMALL Tools & Repairs PO" },
 
             //CET, company "T"
-            new GLAcct { ApexCompany = "T", ApexGLRef = "M", QBGLAcctFullName = "Cost of Goods Sold:H - MATERIALS:Materials - PO" },
-            new GLAcct { ApexCompany = "T", ApexGLRef = "R", QBGLAcctFullName = "Cost of Goods Sold:F - RENTAL EQUIPMENT:Rental Equipment - PO" },
-            new GLAcct { ApexCompany = "T", ApexGLRef = "T", QBGLAcctFullName = "Cost of Goods Sold:F - RENTAL EQUIPMENT:Trailer Rental" },
-            new GLAcct { ApexCompany = "T", ApexGLRef = "S", QBGLAcctFullName = "Cost of Goods Sold:I - SUBCONTRACTS:Subcontracts" },
-            new GLAcct { ApexCompany = "T", ApexGLRef = "E", QBGLAcctFullName = "Cost of Goods Sold:E - TOOLS AND EQUIPMENT:Small Tools & Repairs PO" },
+            new GLAcct { ApexCompany = "T", ApexGLRef = "M", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):H MATERIALS:15 Materials PO" },
+            new GLAcct { ApexCompany = "T", ApexGLRef = "R", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):F RENTAL EQUIPMENT:27 Rental Equipment" },
+            new GLAcct { ApexCompany = "T", ApexGLRef = "T", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):F RENTAL EQUIPMENT:40 Trailer Rental" },
+            new GLAcct { ApexCompany = "T", ApexGLRef = "S", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):I SUBCONTRACTS" },
+            new GLAcct { ApexCompany = "T", ApexGLRef = "E", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):E TOOLS AND EQUIPMENT:35 Small Tools & Repairs PO" },
 
             //CEDB, company "E"
             new GLAcct { ApexCompany = "E", ApexGLRef = "M", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):H MATERIALS:15 Materials PO" },
@@ -42,6 +42,6 @@ namespace ApexQB
             new GLAcct { ApexCompany = "E", ApexGLRef = "S", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):I SUBCONTRACTS" },
             new GLAcct { ApexCompany = "E", ApexGLRef = "E", QBGLAcctFullName = "Cost of Goods Sold (Job Cost):E TOOLS AND EQUIPMENT:35 Small Tools & Repairs PO" }
          };
-      }
-   }
+        }
+    }
 }
